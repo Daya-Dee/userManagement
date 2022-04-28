c403e1f476e1e5321db1d4deff0b9106c780abfb
Daya-Dee <104509501+Daya-Dee@users.noreply.github.com>
2022-04-28 09:33:37 -0400

    init commit :0

---
+++ user_managementProject.py
@@ -0,0 +1,21 @@
+start = input("Hello! How can I help you?\n")
+if start == "help":
+    print("find: find a user\nadd: add a user\ndelete: delete a user\nreport: report an incident\n")
+elif start == "find":
+    print("Who are you looking for? ")
+elif start == "add":
+    print("Who would you like to add? Student\nTeacher\nEmployee\nOther\n")
+elif start == "delete":
+    print("Who would you like to delete? Student\nTeacher\nEmployee\nOther\n")
+  
+'''user management :D'''
+    
+'''    
+who are you storing information for? students, employees, etc.
+find -- what would you like to search by? id, first name, last name, grade, position, etc.
+use dictionaries to store all this information
+class student, employee, teacher, etc.    
+jsonsssdsagbtrfdzx
+sql
+'''
+    
